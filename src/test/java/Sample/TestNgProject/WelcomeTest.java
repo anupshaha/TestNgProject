6b552ab79d9cd9ca5e8d60418d00f585bdd98dbb
Anup Shaha <anupshaha81@gmail.com>
2019-12-31 10:07:06 +0530

    TestNg Project for Integration with Jenkins

---
+++ src/test/java/Sample/TestNgProject/WelcomeTest.java
@@ -0,0 +1,11 @@
+package Sample.TestNgProject;
+
+import org.testng.annotations.Test;
+
+public class WelcomeTest {
+  @Test
+  public void PrintWelcomeTest() {
+	  
+	  System.out.println("Welcome to Selenium with Java ....");
+  }
+}
